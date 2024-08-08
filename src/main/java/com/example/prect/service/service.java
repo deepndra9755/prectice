ccf76b3c6a369f399101c9848bd52d8397b1ded2
deependra <deependrachouhan82@gmail.com>
2024-08-08 12:03:12 -0700

    test5

--- src/main/java/com/example/prect/service/service.java
+++ src/main/java/com/example/prect/service/service.java
@@ -2,6 +2,9 @@ package com.example.prect.service;
 
 public class service {
     public static void main(String[] args) {
-
+        System.out.println("args = " + args);
+        System.out.println("args = " + args);
+        System.out.println("args = " + args);
+        System.out.println("args = " + args);
     }
 }
