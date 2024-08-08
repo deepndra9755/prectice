f4dd3144e3e3cd8a8945390d5f4538cef25f3892
deependra <deependrachouhan82@gmail.com>
2024-08-08 11:34:43 -0700

    test

---
+++ src/main/java/com/example/prect/PrectApplication.java
@@ -0,0 +1,13 @@
+package com.example.prect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/prect/PrectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
